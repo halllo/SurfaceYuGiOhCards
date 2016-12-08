b35f0b46f4863fa6c693a04f490b592a184ea5c2
Manuel Naujoks <mnaujoks@gmail.com>
2016-12-08 20:42:52 +0100

    lifepoint counter

--- SurfaceYuGiOhCards/LifepointCounter.xaml.cs
+++ SurfaceYuGiOhCards/LifepointCounter.xaml.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Surface.Presentation.Controls;
 
 namespace SurfaceYuGiOhCards
 {
-	/// <summary>
-	/// Interaction logic for LifepointCounter.xaml
-	/// </summary>
 	public partial class LifepointCounter : UserControl
 	{
 		public LifepointCounter()
 		{
 			InitializeComponent();
+		}
+
+		private void ChangeLifepoints(object sender, RoutedEventArgs e)
+		{
+			var button = sender as SurfaceButton;
+			var offset = int.Parse(button.Content.ToString());
+			var lifepoints = int.Parse(LifepointDisplay.Text);
+			LifepointDisplay.Text = (lifepoints + offset).ToString();
 		}
 	}
 }
